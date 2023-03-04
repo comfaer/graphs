206afdf16a151060266608c1fa902f88b25bb7c2
Maxim Ermakov <ermakov-99@bk.ru>
2023-03-04 15:14:20 +0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
+import * as echarts from 'echarts'
+import {useEffect} from 'react';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  useEffect( ()=> {
+
+    let myChart = echarts.init(document.getElementById('main'));
+
+    let option = {
+        title: {
+            text: 'Проекты в программах и вне программ',
+            subtext: 'Сумма и процентное соотношение проектов, находящихся в программах и вне программ',
+            top: 16, left: 16
+        },
+        tooltip: {
+          width: 191,
+          height: 189,
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: 23,
+          top: 117,
+          width: 728,
+          height: 365,
+          containLabel: true
+        },
+        legend: {
+            data: ['В программе ЦП', 'Вне программ ЦП', 'В программе ИТ', 'Вне программ ИТ'],
+            top: 528,
+            left: 'center',
+            icon: 'circle'
+        },
+        xAxis: [{
+            type: 'category',
+            data: ['Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь']
+        }],
+        yAxis: [{type: 'value'}],
+        series:[
+            {
+                name: 'В программе ЦП',
+                color: '#56B9F2',
+                type: 'bar',
+                stack: 'Ad',
+                label: {
+                  show: 'true',
+                  position: 'top'
+                },
+                emphasis: {
+                  focus: 'series'
+                },
+                data: data.filter(dat=> dat.name === 'В программе ЦП').map(m=>m.value)
+              },
+            {
+              name: 'Вне программ ЦП',
+              color: '#22C38E',
+              type: 'bar',
+              stack: 'Bd',
+              label: {
+                show: 'true',
+                position: 'top'
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: data.filter(dat=> dat.name === 'Вне программ ЦП').map(m=>m.value)
+          },
+
+          {
+            name: 'В программе ИТ',
+            color: '#0078D2',
+            type: 'bar',
+            stack: 'Ad',
+            label: {
+              show: 'true',
+              position: 'top'
+            },
+            data: data.filter(dat=> dat.name === 'В программе ИТ').map(m=>m.value),
+            emphasis: {
+              focus: 'series'
+            }
+          },
+
+          {
+            name: 'Вне программ ИТ',
+            color: '#00724C',
+            type: 'bar',
+            stack: 'Bd',
+            label: {
+              show: 'true',
+              position: 'top'
+            },
+            data: data.filter(dat=> dat.name === 'Вне программ ИТ').map(m=>m.value),
+            emphasis: {
+              focus: 'series'
+            }
+          },
+
+        ]
+    };
+    myChart.setOption(option);
+})
+  return (<div id='main'></div>);
 }
 
 export default App;
